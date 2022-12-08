33ff2d46b457d09ee3bcac5e30f0143a908b1949
qudlaty <106681377+Chriss350@users.noreply.github.com>
2022-12-08 13:28:41 +0100

    fix

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,8 @@
 const txtInput = document.querySelector("#validation-input");
 const maxInputLength = txtInput.dataset.length;
 
-console.log(maxInputLength);
 txtInput.addEventListener("blur", (event) => {
-  if (event.currentTarget.value.length >= maxInputLength) {
+  if (event.currentTarget.value.length == maxInputLength) {
     txtInput.classList.add("valid");
     txtInput.classList.remove("invalid");
   } else {
